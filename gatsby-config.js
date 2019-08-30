2bbb83653daecb98eed9b49db47654a2496b9dea
Victor Amorim <victor.rma@gmail.com>
2019-08-29 22:15:33 -0300

    Clean up initial project

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Varm`,
+    description: `Um blog sobre desenvolvimento, com o foco em qualidade e aprendizado contínuo.`,
+    author: `@victoramorim`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
