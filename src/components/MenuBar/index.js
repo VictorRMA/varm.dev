6ca7ae6d4292c234e5b56c4f8c0cf57b2497e641
Victor Amorim <victor.rma@gmail.com>
2020-04-30 23:00:53 -0300

    Update dependencies

--- src/components/MenuBar/index.js
+++ src/components/MenuBar/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
 
-import { Home } from "styled-icons/boxicons-solid/Home"
-import { SearchAlt2 as Search } from "styled-icons/boxicons-regular/SearchAlt2"
-import { UpArrowAlt as Arrow } from "styled-icons/boxicons-regular/UpArrowAlt"
+import { Home } from "@styled-icons/boxicons-solid/Home"
+import { SearchAlt2 as Search } from "@styled-icons/boxicons-regular/SearchAlt2"
+import { UpArrowAlt as Arrow } from "@styled-icons/boxicons-regular/UpArrowAlt"
 
 import * as S from './styled'
 
